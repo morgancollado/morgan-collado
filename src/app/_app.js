4e9369c24c33a390525004515d69380d614432be
Morgan Collado <morgan.collado@gmail.com>
2023-12-22 17:34:42 -0600

    one more prettier commit for good measure

--- src/app/_app.js
+++ src/app/_app.js
@@ -1,14 +1,17 @@
 // pages/_app.js
-import Head from 'next/head';
-import '../styles/globals.css';
+import Head from "next/head";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;700&display=swap" rel="stylesheet" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
-        <Component {...pageProps} />
+      <Component {...pageProps} />
     </>
   );
 }
